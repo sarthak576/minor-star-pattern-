25107653db40517486a1a2810dd4e999eee38dd9
sarthak <sarthakguptarjn@gmail.com>
2024-08-16 14:49:47 +0530

    "edited"

---
+++ star1.java
@@ -0,0 +1,17 @@
+// print the pattern ? 
+/*
+       ****
+       ****
+       ****
+       ****  
+ */
+public class star1 {
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<=4;i++){
+            System.out.println("*");
+        }
+
+    }
+}
