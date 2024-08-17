138535c45c1c72ba901fbffb423b3e4622544555
sarthak <sarthakguptarjn@gmail.com>
2024-08-17 15:10:46 +0530

    paterns in a single line with line spaces at the end.

--- star1.java
+++ star1.java
@@ -5,13 +5,19 @@
        ****
        ****  
  */
-public class star1 {
-    
+public class star1 {    
     public static void main(String[] args) {
 
         for(int i=0;i<=4;i++){
-            System.out.println("*");
-        }
+for(int j=0;j<=4;j++){
+    System.out.println("*");
+
+}
+System.out.println(" ");
+
+}
+
 
     }
 }
+
